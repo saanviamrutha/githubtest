153261cb491ffcc17bfb20f88aa29b5dadb3cab0
saanviamrutha <102648734+saanviamrutha@users.noreply.github.com>
2022-04-06 15:17:56 +0530

    Add files via upload

--- codes/pcode.py
+++ codes/pcode.py
@@ -1,1 +1,11 @@
+Angle_BAD=90
+Angle_ADB=55
+#Angle_BAD+Angle_ADB+Angle_ADB=180
+Angle_ABD=180-(90+Angle_ADB)
+Angle_AOE=2*Angle_ABD
+y=Angle_AOE
+print("y=",y,chr(176),sep='')
+Angle_OAC=90
+x=180-(y+Angle_OAC)
+print("x=",x,chr(176),sep='')
 
